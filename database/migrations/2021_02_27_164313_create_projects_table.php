1b49603aa3c5172881da7c39ff7ad2f7bc0425f4
Daniel Noyola <daniel.noyola.dev@gmail.com>
2021-02-27 11:32:42 -0600

    text instead of string

--- database/migrations/2021_02_27_164313_create_projects_table.php
+++ database/migrations/2021_02_27_164313_create_projects_table.php
@@ -15,7 +15,7 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->text('name');
             $table->timestamps();
         });
     }
